9727c2f511ae94d568dba9334ffaa3fb598cdb2d
zawawimanja <zawawimanja@gmail.com>
2019-12-12 15:15:37 +0800

    first commit

---
+++ pca10056/s140/ses/Output/ble_app_uart_pca10056_s140 Release/Obj/utf.d
@@ -0,0 +1,6 @@
+Output/ble_app_uart_pca10056_s140\ Release/Obj/utf.o: \
+ C:\Users\amir\Desktop\Desktop\IOT\nRF5_SDK_15.3.0_59ac345\external\utf_converter\utf.c \
+ C:\Users\amir\Desktop\Desktop\IOT\nRF5_SDK_15.3.0_59ac345\external\utf_converter\utf.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.30b/include/stddef.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.30b/include/__crossworks.h \
+ C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.30b/include/stdint.h
